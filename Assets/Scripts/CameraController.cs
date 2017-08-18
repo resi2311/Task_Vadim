2f8a43411a43ce10b3985b92bbae7c9bf8eec037
resi2311 <resi2311@mail.ru>
2017-08-18 16:57:31 +0300

    Init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Vector3 offset;  // Направление на камеру
+	public GameObject Ball;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - Ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = Ball.transform.position + offset;  // Обновление позиции камеры
+	}
+}
